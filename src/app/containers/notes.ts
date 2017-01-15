242b60137f7f917925f8c4a05b2dcfc7dc423c7e
tundet <ryunde.taba@gmail.com>
2017-01-15 18:53:11 +0200

    done

--- src/app/containers/notes.ts
+++ src/app/containers/notes.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core'
+import { NoteService } from '../services';
 
 @Component({
   selector: 'notes-container',
@@ -20,8 +21,8 @@ import { Component } from '@angular/core'
           <note-card
             class="col-xs-4"
             [note]="note"
-            *ngFor="let note of notes; let i = index"
-            (checked)="onNoteChecked(i)"
+            *ngFor="let note of notes"
+            (checked)="onNoteChecked($event)"
           >
           </note-card>
         </div>
@@ -32,11 +33,21 @@ import { Component } from '@angular/core'
 export class Notes {
   notes = [];
 
-  onCreateNote(note) {
-    this.notes.push(note);
+  constructor(private noteService: NoteService){
+    this.noteService.getNotes()
+        .subscribe(resp => this.notes = resp.data);
   }
 
-  onNoteChecked(i) {
-    this.notes.splice(i, 1);
+  onCreateNote(note) {
+    this.noteService.createNote(note)
+        .subscribe(note => this.notes.push(note));
+  }
+
+  onNoteChecked(note) {
+    this.noteService.completeNote(note)
+        .subscribe(note => {
+          const i = this.notes.findIndex(localNote => localNote.id == note.id);
+          this.notes.splice(i, 1);
+        })
   }
 }
