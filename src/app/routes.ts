774b8950d568c0d1950cf8a93190ac6d4fa6c5d7
tundet <ryunde.taba@gmail.com>
2017-01-15 19:11:42 +0200

    done

---
+++ src/app/routes.ts
@@ -0,0 +1,15 @@
+import {RouterModule, Router} from '@angular/router';
+import {ModuleWithProviders} from '@angular/core';
+import { Main, Notes, About } from './containers';
+
+export const routes: ModuleWithProviders = RouterModule.forRoot([
+    {
+        path: '',
+        component: Main,
+        children: [
+            { path: '', component: Notes },
+            { path: 'about', component: About }
+        ]
+    },
+    { path: '**', redirectTo: ''}
+])
--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,38 @@
-import { NgModule }      from '@angular/core'
-import { BrowserModule } from '@angular/platform-browser'
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http'
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
-import { App, providers } from './app'
-import { Main, Notes } from './app/containers'
-import { 
-  AppBar,
-  NoteCard,
-  NoteCreator,
-  ColorPicker
+import {NgModule}      from '@angular/core'
+import {BrowserModule} from '@angular/platform-browser'
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http'
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic'
+import {App, providers, routes} from './app'
+import {Main, Notes, About} from './app/containers'
+import {
+    AppBar,
+    NoteCard,
+    NoteCreator,
+    ColorPicker
 } from './app/ui'
 
 @NgModule({
-  declarations: [
-    App,
-    Main,
-    AppBar,
-    NoteCard,
-    Notes,
-    NoteCreator,
-    ColorPicker
-  ],
-  providers,
-  imports: [BrowserModule, FormsModule, HttpModule],
-  bootstrap: [App]
+    declarations: [
+        App,
+        Main,
+        AppBar,
+        NoteCard,
+        Notes,
+        NoteCreator,
+        ColorPicker,
+        About
+    ],
+    providers,
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        routes
+    ],
+    bootstrap: [App]
 })
-export class AppModule {}
+export class AppModule {
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
