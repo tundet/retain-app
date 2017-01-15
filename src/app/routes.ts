0dbbb135a29b0ab20e3cd7aad989def052b8d375
tundet <ryunde.taba@gmail.com>
2017-01-15 19:45:55 +0200

    done

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,15 +1,18 @@
-import { RouterModule } from '@angular/router';
-import { ModuleWithProviders } from '@angular/core' 
-import { Main, Notes, About } from './containers';
+import {RouterModule} from '@angular/router';
+import {ModuleWithProviders} from '@angular/core'
+import {Main, Notes, About, Auth} from './containers';
+import {AuthService} from './services';
 
 export const routes: ModuleWithProviders = RouterModule.forRoot([
-  {
-    path: '',
-    component: Main,
-    children: [
-      { path: '', component: Notes },
-      { path: 'about', component: About }
-    ]
-  },
-  { path: '**', redirectTo: '' }
+    {
+        path: '',
+        component: Main,
+        canActivate: [AuthService],
+        children: [
+            {path: '', component: Notes},
+            {path: 'about', component: About}
+        ]
+    },
+    {path: 'auth', component: Auth},
+    {path: '**', redirectTo: ''}
 ]);
