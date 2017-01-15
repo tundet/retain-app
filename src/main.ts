5a5415ad99ea1130e8def798958fbba08e5cad43
tundet <ryunde.taba@gmail.com>
2017-01-15 14:03:20 +0200

    done

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,17 @@ import { BrowserModule } from '@angular/platform-browser'
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
 import { App } from './app'
 import { Main } from './app/containers'
+import { AppBar } from './app/ui'
 
 @NgModule({
-  declarations: [
-    App,
-    Main
-  ],
-  imports: [BrowserModule],
-  bootstrap: [App]
+    declarations: [
+        App,
+        Main,
+        AppBar
+    ],
+    imports: [BrowserModule],
+    bootstrap: [App]
 })
-
 export class AppModule {}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
